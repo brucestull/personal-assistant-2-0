d244476aaee1e222fe3b470ccc8eaa3d549419f6
Flynnt Knapp <bruce.stull@gmail.com>
2025-05-29 20:52:16 -0400

    Remove 'value_centric' app from project.

--- value_centric/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ValueCentricConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "value_centric"
-    verbose_name = "Value Centric Life"
